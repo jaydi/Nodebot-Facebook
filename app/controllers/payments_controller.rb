504dc6f348947220403181aa26fa60f059ef943b
Will Lee <jaydi727@gmail.com>
2016-11-16 18:44:18 +0900

    no message

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -6,20 +6,20 @@ class PaymentsController < ApplicationController
 
   def callback
     # TODO
-    # payment = Payment.find(params[:merchant_uid])
-    # if payment.pay_request?
-    #   if params[:status] == "paid"
-    #     payment.succeed_pay!
-    #   else
-    #     payment.fail_pay!
-    #   end
-    # elsif payment.cancel_request?
-    #   if params[:status] == "cancelled"
-    #     payment.succeed_cancel!
-    #   else
-    #     payment.fail_cancel!
-    #   end
-    # end
+    payment = Payment.find(params[:merchant_uid])
+    if payment.pay_request?
+      if params[:status] == "paid"
+        payment.succeed_pay!
+      else
+        payment.fail_pay!
+      end
+    elsif payment.cancel_request?
+      if params[:status] == "cancelled"
+        payment.succeed_cancel!
+      else
+        payment.fail_cancel!
+      end
+    end
     render :nothing => true, :status => 200
   end
 
