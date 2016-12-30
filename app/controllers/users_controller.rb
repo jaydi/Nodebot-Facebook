20afaa152691310f87b5cb9c1a864ce2f205795c
Will Lee <jaydi727@gmail.com>
2016-12-30 16:51:12 +0900

    no message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,20 @@
 class UsersController < ApplicationController
 
   def show_agreements
-    @user_id = params[:user_id]
+    if @user.agreements_accepted?
+      redirect_to payment_path(id: params[:pending_payment_id], vendor: params[:vendor])
+      return
+    end
+
+    @user = User.find(params[:id])
     @pending_payment_id = params[:pending_payment_id]
     @vendor = params[:vendor]
   end
 
   def accept_agreements
+    @user = User.find(params[:id])
     if params[:terms_accepted] and params[:privacy_accepted]
-      user = User.find(params[:user_id])
-      user.update_attributes({agreements_accepted: true})
+      @user.update_attributes({agreements_accepted: true})
       redirect_to payment_path(id: params[:pending_payment_id])
     else
       redirect_to users_agreements_path, flash: {error_message: "모든 항목에 동의해주셔야 합니다."}
