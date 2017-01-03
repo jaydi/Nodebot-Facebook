0cd703cdb5055364386d947fcc7d213f9216b638
Will Lee <jaydi727@gmail.com>
2017-01-03 13:18:49 +0900

    no message

--- config/puma.rb
+++ config/puma.rb
@@ -5,7 +5,7 @@ daemonize true
 app_dir = File.expand_path("../..", __FILE__)
 shared_dir = "#{app_dir}/shared"
 
-# Default to development
+# Default to staging
 rails_env = ENV['RAILS_ENV'] || "staging"
 environment rails_env
 
@@ -20,16 +20,16 @@ pidfile "#{shared_dir}/pids/puma.pid"
 state_path "#{shared_dir}/pids/puma.state"
 activate_control_app
 
-lowlevel_error_handler do |ex, env|
-  Raven.capture_exception(
-    ex,
-    :message => ex.message,
-    :extra => { :puma => env },
-    :culprit => "Puma"
-  )
-  # note the below is just a Rack response
-  [500, {}, ["An error has occurred, and engineers have been informed. Please reload the page. If you continue to have problems, contact admin@kiki.video\n"]]
-end
+# lowlevel_error_handler do |ex, env|
+#   Raven.capture_exception(
+#     ex,
+#     :message => ex.message,
+#     :extra => { :puma => env },
+#     :culprit => "Puma"
+#   )
+#   # note the below is just a Rack response
+#   [500, {}, ["An error has occurred, and engineers have been informed. Please reload the page. If you continue to have problems, contact admin@kiki.video\n"]]
+# end
 
 on_worker_boot do
   require "active_record"
