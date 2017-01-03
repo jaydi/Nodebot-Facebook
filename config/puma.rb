2a77ad2a547049f20f9e90b4335e8b74859cef71
Will Lee <jaydi727@gmail.com>
2017-01-03 13:11:34 +0900

    no message

--- config/puma.rb
+++ config/puma.rb
@@ -20,6 +20,17 @@ pidfile "#{shared_dir}/pids/puma.pid"
 state_path "#{shared_dir}/pids/puma.state"
 activate_control_app
 
+lowlevel_error_handler do |ex, env|
+  Raven.capture_exception(
+    ex,
+    :message => ex.message,
+    :extra => { :puma => env },
+    :culprit => "Puma"
+  )
+  # note the below is just a Rack response
+  [500, {}, ["An error has occurred, and engineers have been informed. Please reload the page. If you continue to have problems, contact admin@kiki.video\n"]]
+end
+
 on_worker_boot do
   require "active_record"
   ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
