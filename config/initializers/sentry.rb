5fabde95db4ba1f33f4600dd65d7c6f5d86cf7fc
Will Lee <jaydi727@gmail.com>
2017-02-10 15:06:10 +0900

    no message

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Raven.configure do |config|
-  config.dsn = 'https://aa23d2cb2e3e462d997fc4374957416f:7151a1e03d084a46b11ada13b2cfd3ec@sentry.io/126065'
+  config.dsn = APP_CONFIG[:sentry_dsn]
   config.environments = ['staging', 'production']
   config.excluded_exceptions = Raven::Configuration::IGNORE_DEFAULT - ["ActiveRecord::RecordNotFound"]
 end
